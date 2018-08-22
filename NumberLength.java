779003eca177908082b1ffe3998a217fa62430c2
Tamil0142 <42166616+Tamil0142@users.noreply.github.com>
2018-08-22 11:22:59 +0530

    Create NumberLength.java

---
+++ NumberLength.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int len=String.valueOf(n).length();
+		System.out.println(len);	
+}}
